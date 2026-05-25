c6444482fdacf7bfdbb8cbb6f93175b72b76ff7e
hourJsDev <hourjs168@gmail.com>
2026-05-25 08:44:10 +0700

    haha

--- src/data/project.ts
+++ src/data/project.ts
@@ -61,19 +61,66 @@ import Project7Image9 from "../assets/project/7/p9.png";
 import Project7Image10 from "../assets/project/7/p10.png";
 import Project7Image11 from "../assets/project/7/p11.png";
 // project 8
-import Project8Image1 from "../assets/project/8/p1.png";
-import Project8Image2 from "../assets/project/8/p22.png";
-import Project8Image3 from "../assets/project/8/p3.png";
-import Project8Image4 from "../assets/project/8/p4.png";
-import Project8Image5 from "../assets/project/8/p5.png";
-import Project8Image6 from "../assets/project/8/p6.png";
-import Project8Image7 from "../assets/project/8/p7.png";
-import Project8Image8 from "../assets/project/8/p8.png";
-import Project8Image9 from "../assets/project/8/p9.png";
-import Project8Image10 from "../assets/project/8/p10.png";
-import Project8Image11 from "../assets/project/8/p11.png";
-import Project8Image12 from "../assets/project/8/p12.png";
-import Project8Image13 from "../assets/project/8/p13.png";
+import Project8Image1 from "../assets/project/8/1.png";
+import Project8Image2 from "../assets/project/8/2.png";
+import Project8Image3 from "../assets/project/8/3.png";
+import Project8Image4 from "../assets/project/8/4.png";
+import Project8Image5 from "../assets/project/8/5.png";
+import Project8Image7 from "../assets/project/8/7.png";
+import Project8Image8 from "../assets/project/8/8.png";
+import Project8Image9 from "../assets/project/8/9.png";
+import Project8Image10 from "../assets/project/8/10.png";
+import Project8Image11 from "../assets/project/8/11.png";
+import Project8Image12 from "../assets/project/8/12.png";
+import Project8Image13 from "../assets/project/8/13.png";
+import Project8Image14 from "../assets/project/8/14.png";
+import Project8Image15 from "../assets/project/8/15.png";
+import Project8Image16 from "../assets/project/8/16.png";
+import Project8Image17 from "../assets/project/8/17.png";
+import Project8Image18 from "../assets/project/8/18.png";
+import Project8Image19 from "../assets/project/8/19.png";
+import Project8Image20 from "../assets/project/8/20.png";
+import Project8Image21 from "../assets/project/8/21.png";
+import Project8Image22 from "../assets/project/8/22.png";
+import Project8Image23 from "../assets/project/8/23.png";
+import Project8Image24 from "../assets/project/8/24.png";
+import Project8Image25 from "../assets/project/8/25.png";
+import Project8Image26 from "../assets/project/8/26.png";
+import Project8Image27 from "../assets/project/8/27.png";
+import Project8Image28 from "../assets/project/8/28.png";
+import Project8Image29 from "../assets/project/8/29.png";
+import Project8Image30 from "../assets/project/8/30.png";
+import Project8Image31 from "../assets/project/8/31.png";
+import Project8Image32 from "../assets/project/8/32.png";
+import Project8Image33 from "../assets/project/8/33.png";
+import Project8Image34 from "../assets/project/8/34.png";
+import Project8Image35 from "../assets/project/8/35.png";
+import Project8Image36 from "../assets/project/8/36.png";
+import Project8Image37 from "../assets/project/8/37.png";
+import Project8Image38 from "../assets/project/8/38.png";
+import Project8Image39 from "../assets/project/8/39.png";
+import Project8Image40 from "../assets/project/8/40.png";
+import Project8Image41 from "../assets/project/8/41.png";
+import Project8Image42 from "../assets/project/8/42.png";
+
+// project 9
+import Project9Image1 from "../assets/project/9/1.png";
+import Project9Image2 from "../assets/project/9/2.png";
+import Project9Image3 from "../assets/project/9/3.png";
+import Project9Image4 from "../assets/project/9/4.png";
+import Project9Image5 from "../assets/project/9/5.png";
+import Project9Image7 from "../assets/project/9/7.png";
+import Project9Image8 from "../assets/project/9/8.png";
+import Project9Image9 from "../assets/project/9/9.png";
+import Project9Image10 from "../assets/project/9/10.png";
+import Project9Image11 from "../assets/project/9/11.png";
+import Project9Image12 from "../assets/project/9/12.png";
+import Project9Image13 from "../assets/project/9/13.png";
+import Project9Image14 from "../assets/project/9/14.png";
+import Project9Image15 from "../assets/project/9/15.png";
+import Project9Image16 from "../assets/project/9/16.jpg";
+import Project9Image17 from "../assets/project/9/17.jpg";
+import Project9Image18 from "../assets/project/9/18.jpg";
 
 export const project = [
   {
@@ -199,11 +246,45 @@ export const project = [
     images: [
       [Project8Image1, Project8Image2],
       [Project8Image3, Project8Image4],
-      [Project8Image5, Project8Image6],
+      [Project8Image5],
       [Project8Image7, Project8Image8],
       [Project8Image9, Project8Image10],
       [Project8Image11, Project8Image12],
-      [Project8Image13],
+      [Project8Image13, Project8Image14],
+      [Project8Image15, Project8Image16],
+      [Project8Image17, Project8Image18],
+      [Project8Image19, Project8Image20],
+      [Project8Image21, Project8Image22],
+      [Project8Image23, Project8Image24],
+      [Project8Image25, Project8Image26],
+      [Project8Image27, Project8Image28],
+      [Project8Image29, Project8Image30],
+      [Project8Image31, Project8Image32],
+      [Project8Image33, Project8Image34],
+      [Project8Image35, Project8Image36],
+      [Project8Image37, Project8Image38],
+      [Project8Image39, Project8Image40],
+      [Project8Image41, Project8Image42],
+    ],
+  },
+  {
+    id: 10,
+    name: "Sketching",
+    image: Project9Image18,
+    year: 2026,
+    subject: "Sketching",
+    program: "Freelance",
+    area: "5000 m2",
+    location: "Bahía Blanca, Argentina",
+    images: [
+      [Project9Image1, Project9Image2],
+      [Project9Image3, Project9Image4],
+      [Project9Image7, Project9Image8],
+      [Project9Image9, Project9Image10],
+      [Project9Image11, Project9Image12],
+      [Project9Image13, Project9Image14],
+      [Project9Image15, Project9Image16],
+      [Project9Image17, Project9Image5],
     ],
   },
 ];
